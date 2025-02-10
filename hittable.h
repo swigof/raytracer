5d8306237c2c4d2999b491413bef3d5ffe601683
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-02-10 15:40:28 -0500

    use bounding volume hierarchy

--- hittable.h
+++ hittable.h
@@ -1,7 +1,7 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
+#include "aabb.h"
 
 class material;
 
@@ -25,6 +25,8 @@ class hittable {
     virtual ~hittable() = default;
 
     virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+    virtual aabb bounding_box() const = 0;
 };
 
 #endif
