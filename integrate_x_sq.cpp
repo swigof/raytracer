4ecad47bbb137c5c6350bb0c6455f3b3e8125386
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-02-18 16:50:32 -0500

    add PDF estimation

--- integrate_x_sq.cpp
+++ integrate_x_sq.cpp
@@ -3,17 +3,27 @@
 #include <iostream>
 #include <iomanip>
 
+double icd(double d) {
+    return std::sqrt(4.0 * d);
+}
+
+double pdf(double x) {
+    return x / 2.0;
+}
+
 int main() {
-    int a = 0;
-    int b = 2;
     int N = 1000000;
     auto sum = 0.0;
 
     for (int i = 0; i < N; i++) {
-        auto x = random_double(a, b);
-        sum += x*x;
+        auto z = random_double();
+        if (z == 0.0)  // Ignore zero to avoid NaNs
+            continue;
+
+        auto x = icd(z);
+        sum += x*x / pdf(x); // counter-weight the nonuniformity
     }
 
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << (b - a) * (sum / N) << '\n';
+    std::cout << "I = " << (sum / N) << '\n';
 }
